9bc3025979a5cf8ca70322d4a71125afa05d79f2
maxscores <maxstackhouse@gmail.com>
2018-02-17 15:59:23 -0700

    Add Form Route for Editing

--- app/controllers/solar_controller.rb
+++ app/controllers/solar_controller.rb
@@ -3,4 +3,8 @@ class SolarController < ApplicationController
     @estimate = NrelService.new().get_estimated_performance(params)
     @utility_rates = OpenEiService.new().get_utility_rates(params)
   end
+
+  def form
+    render "solar/form"
+  end
 end
