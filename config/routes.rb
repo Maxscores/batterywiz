1fe26169b610b197924e74f0279cd7f575e33e9c
maxscores <maxstackhouse@gmail.com>
2018-02-20 16:22:53 -0700

    update routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
   namespace :battery_wizard do
-    get '/installation/new', to: 'battery_wizard/installation#new', as: 'new_installation'
-    get '/summary', to: 'battery_wizard/installation#show', as: 'summary'
-    get '/consumption', to: 'battery_wizard/consumption#new', as: 'consumption'
-    get '/system', to: 'battery_wizard/system#new', as: "system"
-
+    resources :installation, only: [:show, :new, :create]
+    resources :consumption, only: [:new]
+    resources :system, only: [:new]
   end
 end
