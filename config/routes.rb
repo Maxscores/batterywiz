b582ef6f826d8357d013d7d640c8e10a82e83a92
maxscores <maxstackhouse@gmail.com>
2018-02-20 13:31:10 -0700

    add new installation path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
+  get '/installation/new', to: 'solar#new', as: 'new_installation'
   get '/consumption', to: 'solar#consumption', as: 'consumption'
   get '/system', to: 'solar#system', as: "system"
   get '/summary', to: 'solar#show', as: 'summary'
