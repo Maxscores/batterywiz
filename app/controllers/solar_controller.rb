db6560aa6280f8c8c36f56159f4b48e1166fd935
maxscores <maxstackhouse@gmail.com>
2018-02-20 12:40:59 -0700

    Comment services for summary controller

--- app/controllers/solar_controller.rb
+++ app/controllers/solar_controller.rb
@@ -1,7 +1,7 @@
 class SolarController < ApplicationController
   def show
-    @estimate = NrelService.new().get_estimated_performance(params)
-    @utility_rates = OpenEiService.new().get_utility_rates(params)
+    # @estimate = NrelService.new().get_estimated_performance(params)
+    # @utility_rates = OpenEiService.new().get_utility_rates(params)
   end
 
   def consumption
