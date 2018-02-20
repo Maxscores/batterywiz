9588dfce5e145a9cd8fef7f6c88607df027140ab
maxscores <maxstackhouse@gmail.com>
2018-02-20 16:40:52 -0700

    Add battery consumption new spec

--- spec/features/battery_wizard/user_starts_battery_wizard_spec.rb
+++ spec/features/battery_wizard/user_starts_battery_wizard_spec.rb
@@ -16,4 +16,38 @@ describe "As a user that clicks on battery calculator from the home page" do
       expect(Installation.last.zipcode).to eq("99999")
     end
   end
+
+  context "I have filled in my installation location" do
+    describe "and am on the consumption profile page" do
+      it "I enter my consumption by month information", js: true do
+        create_list(:category, 2)
+
+        visit '/'
+
+        click_on 'Battery Calculator'
+
+        fill_in "installation[zipcode]", with: "80525"
+        click_on "Begin"
+
+        click_on "Utility Bill"
+        fill_in "consumption[january]", with: 100
+        fill_in "consumption[february]", with: 100
+        fill_in "consumption[march]", with: 100
+        fill_in "consumption[april]", with: 100
+        fill_in "consumption[may]", with: 100
+        fill_in "consumption[june]", with: 100
+        fill_in "consumption[july]", with: 100
+        fill_in "consumption[august]", with: 100
+        fill_in "consumption[september]", with: 100
+        fill_in "consumption[october]", with: 100
+        fill_in "consumption[november]", with: 100
+        fill_in "consumption[december]", with: 100
+
+        click_on "Next"
+
+        expect(current_path).to eq("/battery_wizard/system/new")
+        expect(Consumption.last.jan).to eq(100)
+      end
+    end
+  end
 end
