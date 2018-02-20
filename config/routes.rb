97aa4a7496908b6d6a6002db89b2691281b62023
maxscores <maxstackhouse@gmail.com>
2018-02-20 09:41:30 -0700

    Update Routes with each step of wizard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  get '/estimate', to: 'solar#show', as: 'estimate'
-  get '/form', to: 'solar#form'
+  get '/consumption', to: 'solar#consumption', as: 'consumption'
+  get '/system', to: 'solar#system', as: "system"
+  get '/summary', to: 'solar#show', as: 'summary'
 end
