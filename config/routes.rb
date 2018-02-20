bed007b78836b58eeea31b8af2ad06cc139dbdd7
maxscores <maxstackhouse@gmail.com>
2018-02-20 15:35:03 -0700

    Namespace wizard with controllers for each resource created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  get '/installation/new', to: 'battery_wizard#new', as: 'new_installation'
-  get '/consumption', to: 'battery_wizard#consumption', as: 'consumption'
-  get '/system', to: 'battery_wizard#system', as: "system"
-  get '/summary', to: 'battery_wizard#show', as: 'summary'
+  namespace :battery_wizard do
+    get '/installation/new', to: 'battery_wizard/installation#new', as: 'new_installation'
+    get '/summary', to: 'battery_wizard/installation#show', as: 'summary'
+    get '/consumption', to: 'battery_wizard/consumption#new', as: 'consumption'
+    get '/system', to: 'battery_wizard/system#new', as: "system"
+
+  end
 end
