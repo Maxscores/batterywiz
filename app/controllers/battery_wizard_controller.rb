b67cb4aaa5c61681d6156fb22060826cdd9b7d5e
Max Stackhouse <maxstackhouse@gmail.com>
2018-02-20 13:36:57 -0700

    Summary page (#16)
    * Add buttons to top-nav
    
    * add summary show page
    
    * Change index to hero page
    
    * Update top nav with installation new path
    
    * add new installation to solar controller
    
    * Add new installation view
    
    * Add Summary View
    
    * add new installation path
    
    * Change name to BatteryWizard
    
    * Fix all battery wizard stuff

--- app/controllers/battery_wizard_controller.rb
+++ app/controllers/battery_wizard_controller.rb
@@ -1,16 +1,20 @@
-class SolarController < ApplicationController
+class BatteryWizardController < ApplicationController
   def show
     # @estimate = NrelService.new().get_estimated_performance(params)
     # @utility_rates = OpenEiService.new().get_utility_rates(params)
   end
 
+  def new
+    render "battery_wizard/installation"
+  end
+
   def consumption
     @categories = Category.all
-    render "solar/consumption"
+    render "battery_wizard/consumption"
   end
 
   def system
-    render "solar/system"
+    render "battery_wizard/system"
   end
 
 end
