04cd23da501383bd5394d2486aeb9613c0f2b697
maxscores <maxstackhouse@gmail.com>
2018-02-21 15:42:13 -0700

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  resources :battery, only: [:new, :create]
+  resources :battery, only: [:show, :new, :create]
 end
