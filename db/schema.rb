ad166fb6dd832ac8fc8d4c764494f3ae3b25f355
maxscores <maxstackhouse@gmail.com>
2018-02-22 11:56:51 -0700

    Add User_ID to installation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222184718) do
+ActiveRecord::Schema.define(version: 20180222185446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20180222184718) do
     t.decimal "utility_rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_installations_on_user_id"
   end
 
   create_table "productions", force: :cascade do |t|
