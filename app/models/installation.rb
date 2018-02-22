e876ed0d976c78c1158eacaf41c4b5b65ba462cb
maxscores <maxstackhouse@gmail.com>
2018-02-22 09:36:01 -0700

    Add Production Model

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -2,4 +2,6 @@ class Installation < ApplicationRecord
   validates_presence_of :zipcode
   has_one :solar_system
   has_one :consumption
+  has_one :production
+
 end
--- app/models/production.rb
+++ app/models/production.rb
@@ -1,40 +1,28 @@
-class Production
-  attr_reader :station,
-              :station_city,
-              :station_state,
-              :jan,
-              :feb,
-              :mar,
-              :apr,
-              :may,
-              :jun,
-              :jul,
-              :aug,
-              :sep,
-              :oct,
-              :nov,
-              :dec
-
-  def initialize(params)
-    @station = params[:station_info][:location]
-    @station_city = params[:station_info][:city]
-    @station_state = params[:station_info][:state]
-    @jan = params[:outputs][:ac_monthly][0].round(2)
-    @feb = params[:outputs][:ac_monthly][1].round(2)
-    @mar = params[:outputs][:ac_monthly][2].round(2)
-    @apr = params[:outputs][:ac_monthly][3].round(2)
-    @may = params[:outputs][:ac_monthly][4].round(2)
-    @jun = params[:outputs][:ac_monthly][5].round(2)
-    @jul = params[:outputs][:ac_monthly][6].round(2)
-    @aug = params[:outputs][:ac_monthly][7].round(2)
-    @sep = params[:outputs][:ac_monthly][8].round(2)
-    @oct = params[:outputs][:ac_monthly][9].round(2)
-    @nov = params[:outputs][:ac_monthly][10].round(2)
-    @dec = params[:outputs][:ac_monthly][11].round(2)
-  end
-
-  def self.get(params)
-    Production.new(NrelService.get_estimated_performance(params))
-  end
-
+class Production < ApplicationRecord
+  belongs_to :installation
+  validates_presence_of :station,
+                        :station_city,
+                        :station_state,
+                        :ac_jan,
+                        :ac_feb,
+                        :ac_mar,
+                        :ac_apr,
+                        :ac_may,
+                        :ac_jun,
+                        :ac_jul,
+                        :ac_aug,
+                        :ac_sep,
+                        :ac_nov,
+                        :ac_dec,
+                        :dc_jan,
+                        :dc_feb,
+                        :dc_mar,
+                        :dc_apr,
+                        :dc_may,
+                        :dc_jun,
+                        :dc_jul,
+                        :dc_aug,
+                        :dc_sep,
+                        :dc_nov,
+                        :dc_dec
 end
