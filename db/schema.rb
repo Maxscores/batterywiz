4ce69a7ebf5af507745868d27f056cf3d386f3c8
maxscores <maxstackhouse@gmail.com>
2018-02-22 08:50:35 -0700

    Create Basic Utility Rate Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221221744) do
+ActiveRecord::Schema.define(version: 20180222150950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20180221221744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_appliances_on_category_id"
+  end
+
+  create_table "basic_utility_rates", force: :cascade do |t|
+    t.decimal "residential"
+    t.decimal "commercial"
+    t.decimal "industrial"
+    t.string "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
