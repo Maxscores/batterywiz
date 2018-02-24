0b259242b6ab8fc4321ddd6e50fa637571f1b89b
maxscores <maxstackhouse@gmail.com>
2018-02-23 17:19:48 -0700

    Refactor Installation Presenter

--- app/models/production.rb
+++ app/models/production.rb
@@ -25,4 +25,22 @@ class Production < ApplicationRecord
                         :dc_sep,
                         :dc_nov,
                         :dc_dec
+
+  def self.find_or_get_production_data(system_details)
+    if installation.production
+      installation.production
+    else
+      production = ProductionBuilder.build do |production|
+        projection = NrelService.get_estimated_performance(system_details)
+        production.set_installation(installation)
+        production.set_station(projection[:station_info][:location])
+        production.set_station_city(projection[:station_info][:city])
+        production.set_station_state(projection[:station_info][:state])
+        production.set_ac_output(projection[:outputs][:ac_monthly])
+        production.set_dc_output(projection[:outputs][:dc_monthly])
+      end
+      production.save!
+      production
+    end
+  end
 end
