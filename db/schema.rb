d888560c3c35de41710cce3ebf06a054e8a6074a
maxscores <maxstackhouse@gmail.com>
2018-02-23 20:18:16 -0700

    Add index to basic utility rates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224000755) do
+ActiveRecord::Schema.define(version: 20180224001103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180224000755) do
     t.string "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["zipcode"], name: "index_basic_utility_rates_on_zipcode"
   end
 
   create_table "categories", force: :cascade do |t|
