11b262952b52bce5b1180327c8449072f567626a
maxscores <maxstackhouse@gmail.com>
2018-02-23 20:21:17 -0700

    Deactivate jQuery Methods

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,46 +45,78 @@ function showPart3() {
   $('#nav-system').addClass('active')
 }
 
+var zipcode = 0;
+
+function getMonthlySolarOutput() {
+  var domain = window.location.origin;
+  var monthlySolarOutput = [];
+  var endpoint = domain + "/api/v1/monthly_solar?zipcode=" + zipcode
+      $.getJSON(endpoint, function(data) {
+        monthlySolarOutput.push(+data["jan"])
+        monthlySolarOutput.push(+data["feb"])
+        monthlySolarOutput.push(+data["mar"])
+        monthlySolarOutput.push(+data["apr"])
+        monthlySolarOutput.push(+data["may"])
+        monthlySolarOutput.push(+data["jun"])
+        monthlySolarOutput.push(+data["jul"])
+        monthlySolarOutput.push(+data["aug"])
+        monthlySolarOutput.push(+data["sep"])
+        monthlySolarOutput.push(+data["oct"])
+        monthlySolarOutput.push(+data["nov"])
+        monthlySolarOutput.push(+data["dec"])
+      });
+  return monthlySolarOutput
+}
+
 function calculateArraySize() {
+  var monthlySolarOutput = getMonthlySolarOutput();
   var monthlyConsumption = [];
-  $(".data-table").find("input").each(function() {
-    monthlyConsumption.push($(this).val());
+  $(".data-table").find("input").each(function(index, value) {
+    var dailySolarOutput = monthlySolarOutput[]
+    var avgDailyConsumption = $(value).val() / 30
+    var requiredSurfaceArea = avgDailyConsumption / dailySolarOutput
+    monthlyConsumption.push(requiredSurfaceArea);
   });
   monthlyConsumption.sort(function(a,b) {
     return a - b;
   });
   monthlyConsumption.reverse();
-  var arraySize = monthlyConsumption[0] * 0.007670;
+  var arraySize = monthlyConsumption[0] / 7;
   return round(arraySize, 1);
 };
 
 $(document).ready(function() {
-  $('.part-1').on('click', '.to-step-2', function() {
-    showPart2()
-  });
-  $('.part-1').on('keydown', '.to-step-2', function() {
-    if (event.keyCode === 13) {
-      showPart2()
-    };
-  });
-  $('.part-2').on('click', '.to-step-3', function() {
-    $("#system-capacity").val(calculateArraySize())
-    showPart3()
-  });
-  $('.part-2').on('keydown', '.to-step-3', function() {
-    if (event.keyCode === 13) {
-      showPart3()
-    };
-  });
-  $('#nav-location').on('click', function(){
-    showPart1()
-  });
-  $('#nav-consumption').on('click', function(){
-    showPart2()
-  });
-  $('#nav-system').on('click', function(){
-    showPart3()
-  });
+  // $('#installation_zipcode').on('keyup', function() {
+  //   zipcode = $('#installation_zipcode').val()
+  // })
+  // $('.part-1').on('click', '.to-step-2', function() {
+  //   window.zipcode = $('input#installation_zipcode').val();
+  //   showPart2()
+  // });
+  // $('.part-1').on('keydown', '.to-step-2', function() {
+  //   if (event.keyCode === 13) {
+  //     window.zipcode = $('input#installation_zipcode').val();
+  //     showPart2()
+  //   };
+  // });
+  // $('.part-2').on('click', '.to-step-3', function() {
+  //   $("#system-capacity").val(calculateArraySize())
+  //   showPart3()
+  // });
+  // $('.part-2').on('keydown', '.to-step-3', function() {
+  //   if (event.keyCode === 13) {
+  //     showPart3()
+  //   };
+  // });
+  // $('#nav-location').on('click', function(){
+  //   showPart1()
+  // });
+  // $('#nav-consumption').on('click', function(){
+  //   showPart2()
+  // });
+  // $('#nav-system').on('click', function(){
+  //   showPart3()
+  // });
 
   $('.section').on('click', '.button', function() {
     $(this).parent().find('.section-text').slideToggle();
