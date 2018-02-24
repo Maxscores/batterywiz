4e0b52c2038fcf85f536cd4236d5c41015aee397
maxscores <maxstackhouse@gmail.com>
2018-02-23 17:19:35 -0700

    Add Monthly Solar Outputs Model

---
+++ app/models/monthly_solar_output.rb
@@ -0,0 +1,2 @@
+class MonthlySolarOutput < ApplicationRecord
+end
---
+++ spec/models/monthly_solar_output_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe MonthlySolarOutput, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
