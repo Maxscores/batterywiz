c397304f095a1b8c0b18d2516e78095831292654
maxscores <maxstackhouse@gmail.com>
2018-02-23 17:31:53 -0700

    Add Find or get by zipcode to monthly solar output

--- app/models/monthly_solar_output.rb
+++ app/models/monthly_solar_output.rb
@@ -1,2 +1,22 @@
 class MonthlySolarOutput < ApplicationRecord
+  def self.find_or_get_by_zipcode(zipcode)
+    if solar_output = MonthlySolarOutput.find_by(zipcode: zipcode)
+      solar_output
+    else
+      data = NrelService.get_monthly_solar_output(zipcode)
+      MonthlySolarOutput.create(zipcode: zipcode,
+        jan: data[:outputs][:avg_dni][:monthly][:jan],
+        feb: data[:outputs][:avg_dni][:monthly][:feb],
+        mar: data[:outputs][:avg_dni][:monthly][:mar],
+        apr: data[:outputs][:avg_dni][:monthly][:apr],
+        may: data[:outputs][:avg_dni][:monthly][:may],
+        jun: data[:outputs][:avg_dni][:monthly][:jun],
+        jul: data[:outputs][:avg_dni][:monthly][:jul],
+        aug: data[:outputs][:avg_dni][:monthly][:aug],
+        sep: data[:outputs][:avg_dni][:monthly][:sep],
+        oct: data[:outputs][:avg_dni][:monthly][:oct],
+        nov: data[:outputs][:avg_dni][:monthly][:nov],
+        dec: data[:outputs][:avg_dni][:monthly][:dec])
+    end
+  end
 end
