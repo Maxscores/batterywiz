93c15bacb1e65e6c4a8a956173396bbc2fb0ac78
maxscores <maxstackhouse@gmail.com>
2018-02-23 20:20:56 -0700

    Remove API Controller

--- app/controllers/api/v1/monthly_solar_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::MonthlySolarController < Api::V1::BaseController
-  def show
-    monthly_solar_output = MonthlySolarOutput.find_or_get_by_zipcode(params[:zipcode])
-    render json: monthly_solar_output
-  end
-end
