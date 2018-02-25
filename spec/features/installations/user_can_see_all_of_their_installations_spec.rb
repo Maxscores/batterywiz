ce8cfbe0e0140dfe86167e194d67a047afa67039
maxscores <maxstackhouse@gmail.com>
2018-02-25 01:53:18 -0700

    Add User Can visit show page from "my installations"

--- spec/features/installations/user_can_see_all_of_their_installations_spec.rb
+++ spec/features/installations/user_can_see_all_of_their_installations_spec.rb
@@ -11,7 +11,9 @@ describe "A logged in user with multiple installations" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       allow_any_instance_of(ApplicationController).to receive(:authenticate_user!).and_return(true)
 
-      visit installations_path
+      visit root_path
+
+      click_on "My Installations"
 
       expect(current_path).to eq("/installations")
       expect(page).to have_link(i_1.id)
@@ -21,6 +23,36 @@ describe "A logged in user with multiple installations" do
       expect(page).to have_link(i_2.id)
       expect(page).to have_content(i_2.zipcode)
       expect(page).to have_content(i_2.solar_system.capacity)
+    end
+
+    it "can click on an installation and see the pace " do
+      VCR.use_cassette("installations/user_sees_one_installation") do      
+        user = create(:user)
+        i_1, i_2 = create_list(:installation, 2, user: user)
+        create(:solar_system, installation: i_1 )
+        create(:solar_system, installation: i_2 )
+        create(:consumption, installation: i_1 )
+        create(:consumption, installation: i_2 )
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        allow_any_instance_of(ApplicationController).to receive(:authenticate_user!).and_return(true)
+
+        visit root_path
+
+        click_on "My Installations"
+
+        click_on "#{i_1.id}"
+
+        expect(current_path).to eq("/installations/#{i_1.id}")
+
+        expect(page).to have_content("Solar System Capacity: ")
+        expect(page).to have_content("Battery Capacity: ")
+        expect(page).to have_content("Azimuth Angle: ")
+        expect(page).to have_content("Panel Tilt: ")
+        expect(page).to have_content("Array Type: ")
+        expect(page).to have_content("Module Type: ")
+        expect(page).to have_content("Losses: ")
+      end
     end
   end
 end
