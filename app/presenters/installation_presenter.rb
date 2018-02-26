351fe3f7ac0e902e7e88b529a01ae6e7f1415e6c
maxscores <maxstackhouse@gmail.com>
2018-02-26 13:20:50 -0700

    Refactor Instalation presenter

--- app/presenters/installation_presenter.rb
+++ app/presenters/installation_presenter.rb
@@ -1,19 +1,11 @@
 class InstallationPresenter
-  attr_reader :installation, :solar_system, :consumption
+  attr_reader :installation, :solar_system, :consumption, :production, :utility_rate
 
   def initialize(installation_id)
     @installation = Installation.includes(:solar_system, :consumption, :production).find(installation_id)
     @solar_system = @installation.solar_system
     @consumption = @installation.consumption
-    @production = nil
-    @utility_rate = nil
-  end
-
-  def production
-    @production ||= @installation.find_or_get_production_data
-  end
-
-  def utility_rate
-    @utility_rate ||= BasicUtilityRate.find_or_get_utility_rate_by_zipcode(installation.zipcode)
+    @production = @installation.production
+    @utility_rate = BasicUtilityRate.find_or_get_utility_rate_by_zipcode(installation.zipcode)
   end
 end
