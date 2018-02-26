853250199c845a1b86f0e01ad0140188fce056df
maxscores <maxstackhouse@gmail.com>
2018-02-26 08:32:09 -0700

    Add edit and update routes

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -33,10 +33,26 @@ class InstallationsController < ApplicationController
     end
   end
 
+  def edit
+    if current_user && current_user.installations.exists?(params[:id])
+      @installation = Installation.find(params[:id])
+    else
+      not_found
+    end
+  end
+
+  def update
+    if Installation.find(params[:id]).update(installation_params)
+      flash[:success] = "The Installation was Successfully Updated"
+      redirect_to installations_path
+    else
+      flash[:message] = "The Installation was unable to be updated"
+      redirect_to edit_installation_path(params[:id])
+    end
+  end
+
   private
     def installation_params
       params.require(:installation).permit(:zipcode)
     end
-
-
 end
