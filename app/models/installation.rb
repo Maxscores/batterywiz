2e3a08b801fb4665a4dd383b1dfbdcd385cc9fec
maxscores <maxstackhouse@gmail.com>
2018-02-26 13:21:52 -0700

    Remove find or get production data from Installation.
    Uses before save on solar_system instead

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -5,25 +5,6 @@ class Installation < ApplicationRecord
   has_one :consumption
   has_one :production
 
-  def find_or_get_production_data
-    system_details = solar_system.attributes.merge!('zipcode' => zipcode)
-    if production
-      production
-    else
-      production = ProductionBuilder.build do |production|
-        projection = NrelService.get_estimated_performance(system_details.symbolize_keys)
-        production.set_installation(self)
-        production.set_station(projection[:station_info][:location])
-        production.set_station_city(projection[:station_info][:city])
-        production.set_station_state(projection[:station_info][:state])
-        production.set_ac_output(projection[:outputs][:ac_monthly])
-        production.set_dc_output(projection[:outputs][:dc_monthly])
-      end
-      production.save!
-      production
-    end
-  end
-
   def calculate_array_size
     average_daily_solar_production = MonthlySolarOutput.find_or_get_by_zipcode(zipcode).avg_daily_production
     grouped = consumption.avg_daily_consumption.zip(average_daily_solar_production)
