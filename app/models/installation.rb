ac8c785d68b9514a5cd6249fd17bf12c4b9bc7bf
maxscores <maxstackhouse@gmail.com>
2018-02-26 20:10:14 -0700

    Add Battery relationship and estimation method

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,9 +1,12 @@
 class Installation < ApplicationRecord
+  include BatteryCalculationModule
+
   validates_presence_of :zipcode
   belongs_to :user, optional: true
   has_one :solar_system
   has_one :consumption
   has_one :production
+  has_one :battery
 
   def calculate_array_size
     average_daily_solar_production = MonthlySolarOutput.find_or_get_by_zipcode(zipcode).avg_daily_production
@@ -11,5 +14,10 @@ class Installation < ApplicationRecord
     grouped.map do |(consumption, production)|
       (1.20 * consumption / production)
     end.max.round(1)
+  end
+
+  def estimate_battery_size
+    estimated_consumption = estimate_consumption(consumption.avg_daily_consumption)
+    calculate_capacity(estimated_consumption, production.hourly_dc)
   end
 end
