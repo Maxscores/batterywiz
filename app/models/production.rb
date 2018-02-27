5d4fcfc4a7487a8677b33a76440949353a03abd8
maxscores <maxstackhouse@gmail.com>
2018-02-27 13:28:35 -0700

    Add Daily production to production

--- app/models/production.rb
+++ app/models/production.rb
@@ -4,4 +4,9 @@ class Production < ApplicationRecord
                         :station_city,
                         :station_state
 
+  def daily_production
+    hourly_dc.each_slice(24).to_a.map do |day_hourly_production|
+      day_hourly_production.sum
+    end
+  end
 end
