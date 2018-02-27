75d9251358215600c9311efa3d483ad64674342c
maxscores <maxstackhouse@gmail.com>
2018-02-27 15:15:39 -0700

    Create Battery Decorator for graphs

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -2,6 +2,6 @@ class Battery < ApplicationRecord
   belongs_to :installation
 
   def battery_level_at_start_of_day(day)
-    daily_net_energy[0..(day-1)].sum
+    daily_net_energy[0..(day-1)].sum / 100
   end
 end
