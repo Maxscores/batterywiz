0c5d09a745a6804996f66413814ad3d660a897eb
maxscores <maxstackhouse@gmail.com>
2018-02-26 20:08:33 -0700

    Addd Battery to presenter

--- app/presenters/installation_presenter.rb
+++ app/presenters/installation_presenter.rb
@@ -1,11 +1,17 @@
 class InstallationPresenter
-  attr_reader :installation, :solar_system, :consumption, :production, :utility_rate
+  attr_reader :installation,
+              :solar_system,
+              :consumption,
+              :production,
+              :utility_rate,
+              :battery
 
   def initialize(installation_id)
-    @installation = Installation.includes(:solar_system, :consumption, :production).find(installation_id)
+    @installation = Installation.includes(:solar_system, :consumption, :production, :battery).find(installation_id)
     @solar_system = @installation.solar_system
     @consumption = @installation.consumption
     @production = @installation.production
     @utility_rate = BasicUtilityRate.find_or_get_utility_rate_by_zipcode(installation.zipcode)
+    @battery = @installation.battery
   end
 end
