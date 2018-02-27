a55bd64a454343a0afaa351c7dc60d3e0bc7ce03
maxscores <maxstackhouse@gmail.com>
2018-02-27 13:28:12 -0700

    Break out methods for battery calculation module

--- app/services/battery_calculation_module.rb
+++ app/services/battery_calculation_module.rb
@@ -1,10 +1,16 @@
 module BatteryCalculationModule
-  def calculate_capacity(hourly_consumption, hourly_production)
+  def calculate_hourly_net_energy(hourly_consumption, hourly_production)
     hourly_consumption.zip(hourly_production.each_slice(24).to_a).map do |(day_hourly_consumption, day_hourly_production)|
       day_hourly_consumption.zip(day_hourly_production).map do |(hour_consumption, hour_production)|
-        (hour_production/1000) - hour_consumption
-      end.sum
-    end.max
+        ((hour_production/1000) - hour_consumption).round(1)
+      end
+    end
+  end
+
+  def calculate_daily_net(hourly_net_energy)
+    hourly_net_energy.map do |day|
+      day.sum.round(1)
+    end
   end
 
   def default_consumption_profile
