f6e77db0af886c03bb7ef30762b5476c5268f8fb
maxscores <maxstackhouse@gmail.com>
2018-02-27 15:15:14 -0700

    Create Production Decorator for graph formatting

---
+++ app/decorators/production_decorator.rb
@@ -0,0 +1,41 @@
+class ProductionDecorator < SimpleDelegator
+  def graphable_daily_production
+    start_day = Time.new("01-01")
+    daily_production.map do |energy|
+      formatted = [start_day, energy/1000]
+      start_day += 1.day
+      formatted
+    end
+  end
+
+  def graph_formatter(day_of_year)
+    start_hour = Time.new("00:00:00")
+    hourly_production_by_day[day_of_year].reduce([]) do |formatted, energy|
+      if formatted.empty?
+        formatted << [start_hour, energy/1000]
+        start_hour += 1.hour
+        formatted
+      else
+        formatted << [start_hour, formatted[-1][1] + energy/1000]
+        start_hour += 1.hour
+        formatted
+      end
+    end
+  end
+
+  def production_on_spring_equinox
+    graph_formatter(78)
+  end
+
+  def production_on_summer_solstice
+    graph_formatter(172)
+  end
+
+  def production_on_fall_equinox
+    graph_formatter(264)
+  end
+
+  def production_on_winter_solstice
+    graph_formatter(354)
+  end
+end
--- app/models/production.rb
+++ app/models/production.rb
@@ -4,6 +4,12 @@ class Production < ApplicationRecord
                         :station_city,
                         :station_state
 
+  def hourly_production_by_day
+    hourly_dc.each_slice(24).to_a.map do |day_hourly_production|
+      day_hourly_production
+    end
+  end
+
   def daily_production
     hourly_dc.each_slice(24).to_a.map do |day_hourly_production|
       day_hourly_production.sum
