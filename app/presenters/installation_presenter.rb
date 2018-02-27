98abb12b159c042e0a5140a540218e1fd828aa12
maxscores <maxstackhouse@gmail.com>
2018-02-27 15:14:54 -0700

    Add Decorators to Installation Presenter

--- app/presenters/installation_presenter.rb
+++ app/presenters/installation_presenter.rb
@@ -9,9 +9,9 @@ class InstallationPresenter
   def initialize(installation_id)
     @installation = Installation.includes(:solar_system, :consumption, :production, :battery).find(installation_id)
     @solar_system = @installation.solar_system
-    @consumption = @installation.consumption
-    @production = @installation.production
+    @consumption = ConsumptionDecorator.new(@installation.consumption)
+    @production = ProductionDecorator.new(@installation.production)
     @utility_rate = BasicUtilityRate.find_or_get_utility_rate_by_zipcode(installation.zipcode)
-    @battery = @installation.battery
+    @battery = BatteryDecorator.new(@installation.battery)
   end
 end
