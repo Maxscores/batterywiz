bd7dbfa6cf97d4b3397e64b51a35c6895df07550
maxscores <maxstackhouse@gmail.com>
2018-02-27 16:25:38 -0700

    Add Carbon Offset and Energy Value to show page

--- app/models/production.rb
+++ app/models/production.rb
@@ -15,4 +15,14 @@ class Production < ApplicationRecord
       day_hourly_production.sum
     end
   end
+
+  def total_production
+    daily_production.sum / 1000
+  end
+
+  def carbon_offset
+    total_production * LBS_CO2_PER_KWH / 2000
+  end
+
+  LBS_CO2_PER_KWH = 1.123
 end
