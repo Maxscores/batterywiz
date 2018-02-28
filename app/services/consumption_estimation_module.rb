08013707e9ffba4dd8cceb46d73061167970992d
maxscores <maxstackhouse@gmail.com>
2018-02-27 22:30:55 -0700

    Rename Consumption Estimation Module
    Was consumption profile module

---
+++ app/services/consumption_estimation_module.rb
@@ -0,0 +1,51 @@
+module ConsumptionEstimationModule
+  def estimate_consumption(avg_daily_consumption)
+    (1..12).map do |month|
+      (1..Time.days_in_month(month)).map do
+        #add winter/summer month selection
+        default_consumption_profile.map do |hour_consumption|
+          avg_daily_consumption[month-1] * hour_consumption
+        end
+      end
+    end.flatten(1)
+  end
+
+  def summer_consumption_profile
+    standardize_consumption_profile([])
+  end
+
+  def winter_consumption_profile
+    standardize_consumption_profile([])
+  end
+
+  def default_consumption_profile
+    standardize_consumption_profile([3.9,
+                                      3.1,
+                                      2.0,
+                                      1.2,
+                                      1.1,
+                                      1.0,
+                                      0.9,
+                                      1.1,
+                                      1.2,
+                                      1.4,
+                                      1.9,
+                                      2.1,
+                                      2.5,
+                                      2.5,
+                                      2.7,
+                                      3.5,
+                                      3.5,
+                                      3.8,
+                                      4.4,
+                                      3.9,
+                                      3.6,
+                                      3.5,
+                                      3.5,
+                                      2.4])
+  end
+
+  def standardize_consumption_profile(profile)
+    profile.map {|num| num / profile.sum}
+  end
+end
