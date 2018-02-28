d09a5cf8884eab1cd6b24185b874d9356f0a9a21
maxscores <maxstackhouse@gmail.com>
2018-02-28 14:34:02 -0700

    Update Battery Level at Start of day w/ charge

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -3,7 +3,6 @@ class Battery < ApplicationRecord
   validates_presence_of :capacity, :daily_net_energy, :hourly_net_energy
 
   def battery_level_at_start_of_day(day)
-    daily_net_energy[0..(day-1)].sum / 100
+    daily_net_energy[0..(day-1)].sum % capacity
   end
-
 end
