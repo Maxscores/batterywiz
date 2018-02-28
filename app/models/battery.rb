38130bf57620ef6a127c59ca4d0a8f3641aba139
maxscores <maxstackhouse@gmail.com>
2018-02-27 18:15:50 -0700

    Add Battery Spec

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -1,5 +1,6 @@
 class Battery < ApplicationRecord
   belongs_to :installation
+  validates_presence_of :capacity, :daily_net_energy, :hourly_net_energy
 
   def battery_level_at_start_of_day(day)
     daily_net_energy[0..(day-1)].sum / 100
--- spec/models/battery_spec.rb
+++ spec/models/battery_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Battery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without capacity" do
+      installation = create(:installation)
+      battery = Battery.new(daily_net_energy: [1.2, 1.3], hourly_net_energy: [2.1, 3.1], installation: installation)
+
+      expect(battery).to be_invalid
+    end
+
+    it "is invalid without daily_net_energy" do
+      installation = create(:installation)
+      battery = Battery.new(capacity: 20, hourly_net_energy: [2.1, 3.1], installation: installation)
+
+      expect(battery).to be_invalid
+    end
+
+    it "is invalid without hourly_net_energy" do
+      installation = create(:installation)
+      battery = Battery.new(capacity: 20, daily_net_energy: [2.1, 3.1], installation: installation)
+
+      expect(battery).to be_invalid
+    end
+
+    it "is invalid with installation" do
+      battery = Battery.new(capacity: 20, daily_net_energy: [2.1, 3.1], hourly_net_energy: [2.1, 3.1])
+
+      expect(battery).to be_invalid
+    end
+
+    it "is valid with all" do
+      installation = create(:installation)
+      battery = Battery.new(capacity: 20, daily_net_energy: [2.1, 3.1], hourly_net_energy: [2.1, 3.1], installation: installation)
+
+      expect(battery).to be_valid
+    end
+  end
+
+  describe "instance methods" do
+    it '#battery_level_at_start_of_day' do
+      installation = create(:installation)
+      battery = create(:battery, capacity: 20, daily_net_energy: [200, 300], hourly_net_energy: [2.1, 3.1], installation: installation)
+
+      expect(battery.battery_level_at_start_of_day(1)).to eq(2)
+    end
+  end
 end
